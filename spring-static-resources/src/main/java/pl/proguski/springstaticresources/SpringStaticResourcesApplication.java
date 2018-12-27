7c32a3719644d7b098e7c5455ea22e2da0fd2ea2
PRoguski <piotr8roguski@gmail.com>
2018-12-28 00:08:36 +0100

    init

---
+++ spring-static-resources/src/main/java/pl/proguski/springstaticresources/SpringStaticResourcesApplication.java
@@ -0,0 +1,14 @@
+package pl.proguski.springstaticresources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStaticResourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStaticResourcesApplication.class, args);
+	}
+
+}
+
